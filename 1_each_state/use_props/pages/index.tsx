7c18b7077150f950b2485f8849116ec21a2a18e8
olt13mm <olt13mm@gmail.com>
2022-11-17 22:52:05 +0900

    test: pritier 설정 적용 테스트중

--- 1_each_state/use_props/pages/index.tsx
+++ 1_each_state/use_props/pages/index.tsx
@@ -1,5 +1,11 @@
+import {users} from '../dummys/users';
+// import { users } from 'dummys/users';
+
+let test = 'ddds';
+
 const Home = () => (
   <>
+    <h1>Hello World!</h1>
     <h1>Hello World!</h1>
   </>
 );
